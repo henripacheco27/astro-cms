39dabf6e6326ea8fb3ec19fdb1d00ac0615a694c
henriquepacheco27 <henriquepacheco27@gmail.com>
2025-02-19 11:41:05 -0300

    commit 3

---
+++ henriquepacheco27/astro-cms/src/lib/api.js
@@ -0,0 +1,33 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+const API_URL = process.env.WP_URL;
+
+async function fetchAPI(query, { variables } = {}) {
+    const headers = { 'Content-Type': 'application/json' };
+    const res = await fetch(API_URL, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ query, variables }),
+    });
+  
+    const json = await res.json();
+    if (json.errors) {
+      console.log(json.errors);
+      throw new Error('Failed to fetch API');
+    }
+  
+    return json.data;
+  }
+
+  export async function getPageBySlug(slug) {
+    const data = await fetchAPI(`
+    {
+      page(id: "${slug}", idType: URI) {
+        title
+        content
+      }
+    }
+    `);
+    return data?.page;
+  }
